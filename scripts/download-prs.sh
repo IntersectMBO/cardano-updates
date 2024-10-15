867154b96fc2d57452b915ddbfef76bff9d415e3
Jordan Millar <jordan.millar@iohk.io>
2024-10-15 16:48:41 -0400

    Update download-prs.sh to only download PRs that have been merged in the given date range

--- scripts/download-prs.sh
+++ scripts/download-prs.sh
@@ -7,39 +7,38 @@ set -euo pipefail
   exit 1
 }
 
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 repository" >&2
-  echo "Example: $0 IntersectMBO/cardano-node" >&2
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 repository_url start_date end_date" >&2
+  echo "Example: $0 IntersectMBO/cardano-node 2024-09-04 2024-09-17" >&2
   exit 1
 fi
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
   date_cmd=gdate
 else
-  date_cmd=date
+  date_cmd=start_date
 fi
 
-repository="$1"
-date="$2"
+repository_url="$1"
+repository_name="$(basename "$repository_url" ".git")"
+start_date="$2"
+end_date="$3"
 
-out_dir="gen/$repository"
+out_dir="gen/$repository_name"
 
 mkdir -p "$out_dir"
 
 temp_json_file="$(mktemp).json"
 
-# Find a reasonable number for -L
-max_pr_number="$(gh pr list --repo "$repository" --state all --search "created:>=$date" -L 1000 --json number | jq length)"
+max_pr_number="$(gh pr list --repo "$repository_url" --state merged --search "merged:$start_date..$end_date" -L 1000 --json number | jq length)"
+
+gh pr list --repo "$repository_url" --state merged --search "merged:$start_date..$end_date" -L "$max_pr_number" \
+    --json number,title,author,createdAt,closedAt,files,mergedAt,baseRefName,url,body > "$temp_json_file"
 
 
-echo "Repository: $repository"
-echo "Date: $date"
+echo "Repository: $repository_url"
+echo "Date: $start_date"
+echo "End Date: $end_date"
 echo "Downloading up to $max_pr_number PRs"
-
-gh pr list --repo "$repository" \
-  -L "$max_pr_number" \
-  --state all \
-  --json number,title,author,createdAt,closedAt,files,mergedAt,baseRefName,url,body \
-  > "$temp_json_file"
 
 cat "$temp_json_file" | yq -P > "$out_dir/download.yaml"
